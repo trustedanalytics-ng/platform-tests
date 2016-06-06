b3ab6a612539c283430472df9cea3f0ccda6ab81
ajuchnie <aleksandra.juchniewicz@intel.com>
2016-06-06 15:34:36 +0200

    Remove dependency on unittest in Ws2kafka2hdfs test

--- project/tests/test_functional/applications/test_ws2kafka2hdfs.py
+++ project/tests/test_functional/applications/test_ws2kafka2hdfs.py
@@ -26,10 +26,10 @@ from configuration import config
 from modules.app_sources import AppSources
 from modules.constants import ServiceLabels, TapComponent as TAP, TapGitHub
 from modules.hdfs import Hdfs
-from modules.runner.tap_test_case import TapTestCase
 from modules.markers import components, incremental, priority
+from modules.tap_logger import step
 from modules.tap_object_model import Application, ServiceInstance, Upsi
-from tests.fixtures import fixtures, test_data
+from tests.fixtures import fixtures
 
 
 logged_components = (TAP.ingestion_ws_kafka_hdfs, TAP.service_catalog)
@@ -38,7 +38,7 @@ pytestmark = [components.ingestion_ws_kafka_hdfs, components.service_catalog]
 
 @incremental
 @priority.medium
-class Ws2kafka2hdfs(TapTestCase):
+class TestWs2kafka2hdfs:
 
     REPO_OWNER = TapGitHub.intel_data
     MESSAGE_COUNT = 10
@@ -55,7 +55,7 @@ class Ws2kafka2hdfs(TapTestCase):
 
     @pytest.fixture(scope="class")
     def setup_kafka_zookeeper_hdfs_instances(self, request, test_org, test_space):
-        self.step("Create instances for kafka, zookeeper, hdfs")
+        step("Create instances for kafka, zookeeper, hdfs")
 
         kafka = ServiceInstance.api_create_with_plan_name(org_guid=test_org.guid, space_guid=test_space.guid,
                                                           service_label=ServiceLabels.KAFKA,
@@ -83,25 +83,24 @@ class Ws2kafka2hdfs(TapTestCase):
 
     @pytest.fixture(scope="class", autouse=True)
     def workaround_for_kerberos_service(self, request, test_space, setup_kafka_zookeeper_hdfs_instances):
-        self.step("Get credentials for kerberos-service")
+        step("Get credentials for kerberos-service")
         user_provided_services = Upsi.cf_api_get_list()
         kerb_upsi = next((upsi for upsi in user_provided_services if upsi.name == self.KERBEROS_SERVICE), None)
-        self.assertIsNotNone(kerb_upsi, "{} not found".format(self.KERBEROS_SERVICE))
+        assert kerb_upsi is not None, "{} not found".format(self.KERBEROS_SERVICE)
         credentials = kerb_upsi.credentials
-        self.step("Create user-provided service instance for kerberos-service in test space")
+        step("Create user-provided service instance for kerberos-service in test space")
         Upsi.cf_api_create(name=self.KERBEROS_SERVICE, space_guid=test_space.guid, credentials=credentials)
 
     @retry(AssertionError, tries=5, delay=2)
     def _assert_message_count_in_app_stats(self, app, expected_message_count):
-        self.step("Check that application api returns correct number of consumed messages")
+        step("Check that application api returns correct number of consumed messages")
         msg_count = app.api_request(path=self.ENDPOINT_APP_STATS)[0]["consumedMessages"]
-        self.assertEqual(msg_count, expected_message_count,
-                         "Sent {} messages, collected {}".format(expected_message_count, msg_count))
+        assert msg_count == expected_message_count, "Sent {} messages, collected {}".format(expected_message_count, msg_count)
 
     @pytest.mark.usefixtures("login_to_cf")
     @pytest.mark.bugs("DPNG-5225 [hadoop-utils] remove the need for kerberos-service in non-kerberos envs")
-    def test_step_0_push_ws2kafka2hdfs(self):
-        self.step("Clone and compile ingestion app sources")
+    def test_step_0_push_ws2kafka2hdfs(self, test_space):
+        step("Clone and compile ingestion app sources")
         github_auth = config.CONFIG["github_auth"]
         ingestion_repo = AppSources(repo_name=TapGitHub.ws_kafka_hdfs, repo_owner=self.REPO_OWNER, gh_auth=github_auth)
         ingestion_path = ingestion_repo.clone_or_pull()
@@ -112,13 +111,13 @@ class Ws2kafka2hdfs(TapTestCase):
         postfix = str(int(time.time()))
         self.__class__.topic_name = "topic-{}".format(postfix)
 
-        self.step("Push application ws2kafka")
-        self.__class__.app_ws2kafka = Application.push(space_guid=test_data.TestData.test_space.guid,
+        step("Push application ws2kafka")
+        self.__class__.app_ws2kafka = Application.push(space_guid=test_space.guid,
                                                        source_directory=ws2kafka_path,
                                                        name="ws2kafka-{}".format(postfix),
                                                        env_proxy=config.CONFIG["pushed_app_proxy"])
-        self.step("Push application kafka2hdfs")
-        self.__class__.app_kafka2hdfs = Application.push(space_guid=test_data.TestData.test_space.guid,
+        step("Push application kafka2hdfs")
+        self.__class__.app_kafka2hdfs = Application.push(space_guid=test_space.guid,
                                                          source_directory=kafka2hdfs_path,
                                                          name="kafka2hdfs-{}".format(postfix),
                                                          bound_services=(self.KAFKA_INSTANCE_NAME,
@@ -129,8 +128,8 @@ class Ws2kafka2hdfs(TapTestCase):
                                                               "CONSUMER_GROUP": "group-{}".format(postfix)},
                                                          env_proxy=config.CONFIG["pushed_app_proxy"])
 
-        self.assertTrue(self.app_ws2kafka.is_started, "ws2kafka app is not started")
-        self.assertTrue(self.app_kafka2hdfs.is_started, "kafka2hdfs app is not started")
+        assert self.app_ws2kafka.is_started is True, "ws2kafka app is not started"
+        assert self.app_kafka2hdfs.is_started is True, "kafka2hdfs app is not started"
 
     def test_step_1_send_from_ws2kafka2hdfs(self):
         connection_string = "{}/{}".format(self.app_ws2kafka.urls[0], self.topic_name)
@@ -139,15 +138,15 @@ class Ws2kafka2hdfs(TapTestCase):
 
     @pytest.mark.bugs("DPNG-5173 Cannot access hdfs directories using ec2-user")
     def test_step_2_check_messages_in_hdfs(self):
-        self.step("Get details of broker guid")
+        step("Get details of broker guid")
         broker_guid = self.app_kafka2hdfs.get_credentials("hdfs")["uri"].split("/", 3)[3]
-        self.step("Get messages from hdfs")
+        step("Get messages from hdfs")
         hdfs_messages = self._get_messages_from_hdfs("/" + os.path.join(broker_guid, "from_kafka", self.topic_name))
-        self.step("Check that all sent messages are on hdfs")
-        self.assertUnorderedListEqual(hdfs_messages, self.messages)
+        step("Check that all sent messages are on hdfs")
+        assert sorted(hdfs_messages) == sorted(self.messages)
 
     def _send_ws_messages(self, connection_string):
-        self.step("Send messages to {}".format(connection_string))
+        step("Send messages to {}".format(connection_string))
         ws_opts = {"cert_reqs": ssl.CERT_NONE}
         ws_protocol = "ws://"
         if config.CONFIG["ssl_validation"]:
