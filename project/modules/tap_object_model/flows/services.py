b6d535e67fa11fb56395ef70c6b8098fcbe64db5
Sebastian Parzych <sebastianx.parzych@intel.com>
2016-07-07 17:00:02 +0200

    DPNG-7627 Refactor marketplace tests, remove hdfs
    - Refactor test_marketplace to pytest
    - Refactor (split) method create_and_delete_service_instance_and_keys
      and move it to tap_object_model.flows.services
    - Remove HDFS tests since they're covered in another module

--- project/modules/tap_object_model/flows/services.py
+++ project/modules/tap_object_model/flows/services.py
@@ -14,9 +14,9 @@
 # limitations under the License.
 #
 
-from modules.tap_object_model import ServiceInstance, ServiceKey
 from modules.tap_logger import step
-from tests.fixtures import assertions
+from modules.tap_object_model import ServiceInstance, ServiceKey
+from tests.fixtures.assertions import assert_equal_with_retry
 
 
 def get_service_keys(instance):
@@ -26,38 +26,54 @@ def get_service_keys(instance):
 
 
 def create_instance_and_key_then_delete_key_and_instance(org_guid, space_guid, service_label, plan_guid, plan_name):
-    step("Create instance ({} - {})".format(service_label, plan_name))
-    instance = ServiceInstance.api_create(
-        org_guid=org_guid,
-        space_guid=space_guid,
-        service_label=service_label,
-        service_plan_guid=plan_guid
-    )
-    step("Check that the instance was created ({} - {})".format(service_label, plan_name))
-    instance.ensure_created()
+    step("Testing service {} plan {}".format(service_label, plan_name))
+    instance = create_instance(org_guid, space_guid, service_label, plan_guid)
+
     service_key_exception = None
     try:
-        step("Check that the instance exists in summary and has no keys ({} - {})".format(service_label, plan_name))
-        assertions.assert_equal_with_retry(expected_value=[], callableObj=get_service_keys, instance=instance)
-        step("Create a key for the instance and check it's correct ({} - {})".format(service_label, plan_name))
-        instance_key = ServiceKey.api_create(instance.guid)
-        summary = ServiceInstance.api_get_keys(space_guid)
-        assert summary[instance][0] == instance_key
-        step("Delete key and check that it's deleted ({} - {})".format(service_label, plan_name))
-        instance_key.api_delete()
-        summary = ServiceInstance.api_get_keys(space_guid)
-        assert summary[instance] == []
+        instance_key = create_key(instance, space_guid)
+        delete_key(instance, instance_key, space_guid)
     except AssertionError as e:
         service_key_exception = e
+
     try:
-        step("Delete the instance ({} - {})".format(service_label, plan_name))
-        instance.api_delete()
-        step("Check that the instance was deleted ({} - {})".format(service_label, plan_name))
-        instances = ServiceInstance.api_get_list(space_guid=space_guid)
-        assert instance not in instances
+        delete_instance(instance, space_guid)
     except AssertionError as e:
         if service_key_exception is None:
-            service_key_exception = e
+            raise e
     if service_key_exception is not None:
         raise service_key_exception
 
+
+def create_instance(org_guid, space_guid, service_label, plan_guid, params=None):
+    step("Create service instance")
+    instance = ServiceInstance.api_create(org_guid=org_guid, space_guid=space_guid, service_label=service_label,
+                                          service_plan_guid=plan_guid, params=params)
+    step("Check that the instance was created")
+    instance.ensure_created()
+    return instance
+
+
+def delete_instance(instance, space_guid):
+    step("Delete the instance")
+    instance.api_delete()
+    step("Check that the instance was deleted")
+    instances = ServiceInstance.api_get_list(space_guid=space_guid)
+    assert instance not in instances
+
+
+def create_key(instance, space_guid):
+    step("Check that the instance exists in summary and has no service keys")
+    assert_equal_with_retry(expected_value=[], callableObj=get_service_keys, instance=instance)
+    step("Create a key for the instance and check it's correct")
+    instance_key = ServiceKey.api_create(instance.guid)
+    summary = ServiceInstance.api_get_keys(space_guid)
+    assert summary[instance][0] == instance_key
+    return instance_key
+
+
+def delete_key(instance, instance_key, space_guid):
+    step("Delete key and check that it's deleted")
+    instance_key.api_delete()
+    summary = ServiceInstance.api_get_keys(space_guid)
+    assert summary[instance] == []
--- project/tests/test_functional/services/test_marketplace.py
+++ project/tests/test_functional/services/test_marketplace.py
@@ -21,13 +21,14 @@ import pytest
 from modules.constants import ServiceCatalogHttpStatus as HttpStatus, ParametrizedService, ServiceLabels, ServicePlan, \
     TapComponent as TAP
 from modules.exceptions import UnexpectedResponseError
-from modules.runner.tap_test_case import TapTestCase
 from modules.markers import components, long, priority
 from modules.service_tools.jupyter import Jupyter
-from modules.tap_object_model import ServiceInstance, ServiceKey, ServiceType, Space, User
+from modules.tap_logger import step
+from modules.tap_object_model import ServiceInstance, ServiceType, Space, User
+from modules.tap_object_model.flows.services import create_instance_and_key_then_delete_key_and_instance
 from modules.test_names import generate_test_object_name
-from tests.fixtures.assertions import assert_no_errors
-from tests.fixtures.test_data import TestData
+from tests.fixtures.assertions import assert_no_errors, assert_raises_http_exception, assert_in_with_retry, \
+    assert_unordered_list_equal
 
 
 logged_components = (TAP.service_catalog, TAP.application_broker, TAP.gearpump_broker, TAP.hbase_broker,
@@ -38,80 +39,35 @@ pytestmark = [components.service_catalog, components.application_broker, compone
               components.yarn_broker, components.zookeeper_broker, components.zookeeper_wssb_broker]
 
 
-class MarketplaceServices(TapTestCase):
+class TestMarketplaceServices:
 
     SERVICES_TESTED_SEPARATELY = [ServiceLabels.YARN, ServiceLabels.HDFS, ServiceLabels.HBASE, ServiceLabels.GEARPUMP,
                                   ServiceLabels.H2O, ServiceLabels.SEAHORSE]
 
-    @classmethod
-    @pytest.fixture(scope="class", autouse=True)
-    def marketplace_services(cls, test_org, test_space):
-        cls.test_org = test_org
-        cls.test_space = test_space
-        cls.step("Get list of available services from Marketplace")
-        cls.marketplace = ServiceType.api_get_list_from_marketplace(test_space.guid)
+    @staticmethod
+    @pytest.fixture(scope="class")
+    def marketplace_services(test_space):
+        step("Get list of available services from Marketplace")
+        return ServiceType.api_get_list_from_marketplace(test_space.guid)
 
     @classmethod
     @pytest.fixture(scope="class")
     def non_space_developer_users(cls, request, test_org, test_space, class_context):
-        cls.step("Create space auditor")
+        step("Create space auditor")
         space_auditor = User.api_create_by_adding_to_space(class_context, test_org.guid, test_space.guid,
                                                            roles=User.SPACE_ROLES["auditor"])
         cls.space_auditor_client = space_auditor.login()
-        cls.step("Create space manager client")
+        step("Create space manager client")
         space_manager = User.api_create_by_adding_to_space(class_context, test_org.guid, test_space.guid,
                                                            roles=User.SPACE_ROLES["manager"])
         cls.space_manager_client = space_manager.login()
 
-    def _create_and_delete_service_instance_and_keys(self, org_guid, space_guid, service_label, plan_guid):
-        self.step("Create service instance")
-        instance = ServiceInstance.api_create(
-            org_guid=org_guid,
-            space_guid=space_guid,
-            service_label=service_label,
-            service_plan_guid=plan_guid
-        )
-        self.step("Check that the instance was created")
-        instance.ensure_created()
-        service_key_exception = None
-        try:
-            self.step("Check that the instance exists in summary and has no service keys")
-            self.assertEqualWithinTimeout(timeout=5, expected_result=[], callable_obj=self._get_service_keys,
-                                          sleep=1, instance=instance)
-            self.step("Create a key for the instance and check it's correctness")
-            instance_key = ServiceKey.api_create(instance.guid)
-            summary = ServiceInstance.api_get_keys(space_guid)
-            self.assertEqual(summary[instance][0], instance_key)
-            self.step("Delete key and check that it's deleted")
-            instance_key.api_delete()
-            summary = ServiceInstance.api_get_keys(space_guid)
-            self.assertEqual(summary[instance], [])
-        except AssertionError as e:
-            service_key_exception = e
-        try:
-            self.step("Delete the instance")
-            instance.api_delete()
-            self.step("Check that the instance was deleted")
-            instances = ServiceInstance.api_get_list(space_guid=space_guid)
-            self.assertNotIn(instance, instances)
-        except AssertionError as e:
-            if service_key_exception is not None:
-                raise service_key_exception
-            raise e
-        if service_key_exception is not None:
-            raise service_key_exception
-
-    def _get_service_keys(self, instance):
-        summary = ServiceInstance.api_get_keys(instance.space_guid)
-        self.assertIn(instance, summary)
-        return summary[instance]
-
-    def _create_jupyter_instance_and_login(self, param_key, param_value):
+    def _create_jupyter_instance_and_login(self, param_key, param_value, test_org, test_space):
         param = {param_key: param_value}
-        self.step("Create service instance and check it exist in list")
-        jupyter = Jupyter(self.test_org.guid, self.test_space.guid, params=param)
-        self.assertInWithRetry(jupyter.instance, ServiceInstance.api_get_list, self.test_space.guid)
-        self.step("Get credentials for the new jupyter service instance")
+        step("Create service instance and check it exist in list")
+        jupyter = Jupyter(test_org.guid, test_space.guid, params=param)
+        assert_in_with_retry(jupyter.instance, ServiceInstance.api_get_list, test_space.guid)
+        step("Get credentials for the new jupyter service instance")
         jupyter.get_credentials()
         jupyter.login()
         terminal = jupyter.connect_to_terminal(terminal_no=0)
@@ -119,157 +75,158 @@ class MarketplaceServices(TapTestCase):
         return terminal
 
     @priority.medium
-    def test_check_marketplace_services_list_vs_cloudfoundry(self):
-        self.step("Check that services in cf are the same as in Marketplace")
-        cf_marketplace = ServiceType.cf_api_get_list_from_marketplace_by_space(TestData.test_space.guid)
-        self.assertUnorderedListEqual(self.marketplace, cf_marketplace)
+    def test_check_marketplace_services_list_vs_cloudfoundry(self, test_space, marketplace_services):
+        step("Check that services in cf are the same as in Marketplace")
+        cf_marketplace = ServiceType.cf_api_get_list_from_marketplace_by_space(test_space.guid)
+        assert_unordered_list_equal(marketplace_services, cf_marketplace)
 
     @long
     @priority.high
-    def test_create_and_delete_service_instance_and_keys(self):
+    def test_create_and_delete_service_instance_and_keys(self, test_org, test_space, marketplace_services):
         errors = []
-        for service_type in self.marketplace:
+        for service_type in marketplace_services:
             if service_type.label in self.SERVICES_TESTED_SEPARATELY:
                 continue
             for plan in service_type.service_plans:
                 if ParametrizedService.is_parametrized(label=service_type.label, plan_name=plan["name"]):
                     continue
                 try:
-                    self._create_and_delete_service_instance_and_keys(
-                        TestData.test_org.guid, TestData.test_space.guid, service_type.label, plan["guid"])
+                    create_instance_and_key_then_delete_key_and_instance(
+                        test_org.guid, test_space.guid, service_type.label, plan["guid"], plan["name"])
                 except Exception as e:
                     errors.append(e)
         assert_no_errors(errors)
 
     @priority.low
-    def test_create_instance_with_non_required_parameter(self):
+    def test_create_instance_with_non_required_parameter(self, test_org, test_space):
         param_key = "test_param"
         param_value = "test_value"
-        terminal = self._create_jupyter_instance_and_login(param_key, param_value)
+        terminal = self._create_jupyter_instance_and_login(param_key, param_value, test_org, test_space)
         terminal.send_input("env\n")
         output = "".join(terminal.get_output())
-        self.assertIn("{}={}".format(param_key, param_value), output)
+        assert "{}={}".format(param_key, param_value) in output
 
     @priority.medium
-    def test_cannot_create_service_instance_with_name_of_an_existing_instance(self):
+    def test_cannot_create_service_instance_with_name_of_an_existing_instance(self, test_org, test_space,
+                                                                              marketplace_services):
         existing_name = generate_test_object_name()
-        self.step("Create service instance")
+        step("Create service instance")
         instance = ServiceInstance.api_create_with_plan_name(
-            org_guid=TestData.test_org.guid,
-            space_guid=TestData.test_space.guid,
+            org_guid=test_org.guid,
+            space_guid=test_space.guid,
             service_label=ServiceLabels.KAFKA,
             name=existing_name,
             service_plan_name=ServicePlan.SHARED
         )
-        service_list = ServiceInstance.api_get_list(space_guid=TestData.test_space.guid)
-        self.step("Check that the instance was created")
-        self.assertIn(instance, service_list, "Instance was not created")
+        service_list = ServiceInstance.api_get_list(space_guid=test_space.guid)
+        step("Check that the instance was created")
+        assert instance in service_list, "Instance was not created"
         errors = []
-        for service_type in self.marketplace:
+        for service_type in marketplace_services:
             plan_guid = next(iter(service_type.service_plan_guids))
-            self.step("Try to create {} service instance".format(service_type.label))
-            with self.assertRaises(UnexpectedResponseError) as e:
-                ServiceInstance.api_create(TestData.test_org.guid, TestData.test_space.guid, service_type.label,
+            step("Try to create {} service instance".format(service_type.label))
+            with pytest.raises(UnexpectedResponseError) as e:
+                ServiceInstance.api_create(test_org.guid, test_space.guid, service_type.label,
                                            existing_name, service_plan_guid=plan_guid)
-            if e is None or e.exception.status != HttpStatus.CODE_CONFLICT:
+            if e is None or e.value.status != HttpStatus.CODE_CONFLICT:
                 errors.append("Service '{}' failed to respond with given error status.".format(service_type.label))
         assert_no_errors(errors)
-        self.assertUnorderedListEqual(service_list, ServiceInstance.api_get_list(space_guid=TestData.test_space.guid),
-                                      "Some new services were created")
+        assert_unordered_list_equal(service_list, ServiceInstance.api_get_list(space_guid=test_space.guid),
+                                    "Some new services were created")
 
     @priority.low
     @pytest.mark.bugs("DPNG-5154 Http status 500 when trying to create a service instance without a name")
-    def test_cannot_create_instance_without_a_name(self):
-        expected_instance_list = ServiceInstance.api_get_list(TestData.test_space.guid)
-        self.step("Check that instance cannot be created with empty name")
-        self.assertRaisesUnexpectedResponse(HttpStatus.CODE_BAD_REQUEST, HttpStatus.MSG_BAD_REQUEST,
-                                            ServiceInstance.api_create_with_plan_name, TestData.test_org.guid,
-                                            TestData.test_space.guid, ServiceLabels.KAFKA, "",
-                                            service_plan_name=ServicePlan.SHARED)
-        self.assertUnorderedListEqual(expected_instance_list, ServiceInstance.api_get_list(TestData.test_space.guid),
-                                      "New instance was created")
+    def test_cannot_create_instance_without_a_name(self, test_org, test_space):
+        expected_instance_list = ServiceInstance.api_get_list(test_space.guid)
+        step("Check that instance cannot be created with empty name")
+        assert_raises_http_exception(HttpStatus.CODE_BAD_REQUEST, HttpStatus.MSG_BAD_REQUEST,
+                                     ServiceInstance.api_create_with_plan_name, test_org.guid,
+                                     test_space.guid, ServiceLabels.KAFKA, "",
+                                     service_plan_name=ServicePlan.SHARED)
+        assert_unordered_list_equal(expected_instance_list, ServiceInstance.api_get_list(test_space.guid),
+                                    "New instance was created")
 
     @priority.medium
     @pytest.mark.usefixtures("non_space_developer_users")
-    def test_cannot_create_instance_as_non_space_developer(self):
+    def test_cannot_create_instance_as_non_space_developer(self, test_org, test_space, marketplace_services):
         test_clients = {"space_auditor": self.space_auditor_client, "space_manager": self.space_manager_client}
         errors = []
-        for service_type, (name, client) in itertools.product(self.marketplace, test_clients.items()):
+        for service_type, (name, client) in itertools.product(marketplace_services, test_clients.items()):
             for plan in service_type.service_plans:
-                with self.assertRaises(UnexpectedResponseError) as e:
-                    ServiceInstance.api_create(TestData.test_org.guid, TestData.test_space.guid, service_type.label,
+                with pytest.raises(UnexpectedResponseError) as e:
+                    ServiceInstance.api_create(test_org.guid, test_space.guid, service_type.label,
                                                service_plan_guid=plan["guid"], client=client)
-                if e is None or e.exception.status != HttpStatus.CODE_FORBIDDEN:
+                if e is None or e.value.status != HttpStatus.CODE_FORBIDDEN:
                     errors.append("Service '{}' failed to respond with given error status.".format(service_type.label))
         assert_no_errors(errors)
 
     @priority.low
     @pytest.mark.bugs("DPNG-6086 Adding service key to H2O instance fails")
-    def test_create_h2o_service_instance_and_keys(self):
+    def test_create_h2o_service_instance_and_keys(self, test_org, test_space, marketplace_services):
         label = ServiceLabels.H2O
-        h2o = next((s for s in self.marketplace if s.label == label), None)
+        h2o = next((s for s in marketplace_services if s.label == label), None)
         if h2o is None:
-            self.skipTest("h2o is not available in Marketplace")
+            pytest.skip("h2o is not available in Marketplace")
         errors = []
         for plan in h2o.service_plans:
             try:
-                self._create_and_delete_service_instance_and_keys(
-                    TestData.test_org.guid, TestData.test_space.guid, label, plan["guid"])
+                create_instance_and_key_then_delete_key_and_instance(
+                    test_org.guid, test_space.guid, label, plan["guid"], plan["name"])
             except Exception as e:
                 errors.append(e)
         assert_no_errors(errors)
 
     @priority.low
     @pytest.mark.bugs("DPNG-3474 Command cf create-service-key does not work for yarn broker")
-    def test_create_yarn_service_instance_and_keys(self):
+    def test_create_yarn_service_instance_and_keys(self, test_org, test_space, marketplace_services):
         label = ServiceLabels.YARN
-        yarn = next(s for s in self.marketplace if s.label == label)
+        yarn = next(s for s in marketplace_services if s.label == label)
         errors = []
         for plan in yarn.service_plans:
             try:
-                self._create_and_delete_service_instance_and_keys(
-                    TestData.test_org.guid, TestData.test_space.guid, label, plan["guid"])
+                create_instance_and_key_then_delete_key_and_instance(
+                    test_org.guid, test_space.guid, label, plan["guid"], plan["name"])
             except Exception as e:
                 errors.append(e)
         assert_no_errors(errors)
 
     @priority.low
     @pytest.mark.bugs("DPNG-2798 Enable HBase broker to use Service Keys")
-    def test_create_hbase_service_instance_and_keys(self):
+    def test_create_hbase_service_instance_and_keys(self, test_org, test_space, marketplace_services):
         label = ServiceLabels.HBASE
-        hbase = next(s for s in self.marketplace if s.label == label)
+        hbase = next(s for s in marketplace_services if s.label == label)
         errors = []
         for plan in hbase.service_plans:
             try:
-                self._create_and_delete_service_instance_and_keys(
-                    TestData.test_org.guid, TestData.test_space.guid, label, plan["guid"])
+                create_instance_and_key_then_delete_key_and_instance(
+                    test_org.guid, test_space.guid, label, plan["guid"], plan["name"])
             except Exception as e:
                 errors.append(e)
         assert_no_errors(errors)
 
     @priority.low
     @pytest.mark.bugs("DPNG-6087 Connection to service catalog timedout - cannot create gearpump instance")
-    def test_create_gearpump_service_instance_and_keys(self):
+    def test_create_gearpump_service_instance_and_keys(self, test_org, test_space, marketplace_services):
         label = ServiceLabels.GEARPUMP
-        gearpump = next(s for s in self.marketplace if s.label == label)
+        gearpump = next(s for s in marketplace_services if s.label == label)
         errors = []
         for plan in gearpump.service_plans:
             try:
-                self._create_and_delete_service_instance_and_keys(
-                    TestData.test_org.guid, TestData.test_space.guid, label, plan["guid"])
+                create_instance_and_key_then_delete_key_and_instance(
+                    test_org.guid, test_space.guid, label, plan["guid"], plan["name"])
             except Exception as e:
                 errors.append(e)
         assert_no_errors(errors)
 
     @priority.low
-    def test_get_service_instance_summary_from_empty_space(self):
-        self.step("Create a service instance in one space")
-        service_type = next(s for s in self.marketplace if s.label == ServiceLabels.KAFKA)
-        ServiceInstance.api_create(org_guid=TestData.test_org.guid, space_guid=TestData.test_space.guid,
+    def test_get_service_instance_summary_from_empty_space(self, test_org, test_space, marketplace_services):
+        step("Create a service instance in one space")
+        service_type = next(s for s in marketplace_services if s.label == ServiceLabels.KAFKA)
+        ServiceInstance.api_create(org_guid=test_org.guid, space_guid=test_space.guid,
                                    service_label=service_type.label,
                                    service_plan_guid=service_type.service_plan_guids[0])
-        test_space = Space.api_create(TestData.test_org)
-        self.step("Get service instance summary in another space")
+        test_space = Space.api_create(test_org)
+        step("Get service instance summary in another space")
         summary = ServiceInstance.api_get_keys(test_space.guid)
-        self.step("Check that service instance summary is empty in the second space")
-        self.assertEqual(summary, {})
+        step("Check that service instance summary is empty in the second space")
+        assert summary == {}
