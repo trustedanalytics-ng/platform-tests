a727d2fb78f01f38d164d044183f5538d08c7c52
pprzekwa <patryk.przekwas@intel.com>
2016-09-12 13:05:24 +0200

    DPNG-8695 Adjust script for test admin creation

--- deploy/add-test-admin.sh
+++ deploy/add-test-admin.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-#
-# Copyright (c) 2016 Intel Corporation
+#Copyright (c) 2016 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,36 +15,16 @@
 #
 
 # usage:
-# add-test-admin <environment, e.g. daily.gotapaas.com> <cf admin pass> <org=trustedanalytics> <space=platform> <password>
+# add-test-admin <environment, e.g. xx.tap.sclab.intel.com> <uaa client pass> <username> <password>
 
 DOMAIN=$1
-CF_ADMIN_PASS=$2
-ORG_NAME=${3:-trustedanalytics}
-SPACE_NAME=${4:-platform}
-USERNAME=trusted.analytics.tester@gmail.com
-PASSWORD=$5
+ADMIN_SECRET=$2
+USERNAME=$3
+PASSWORD=$4
+EMAIL=$USERNAME@example.com
 
-
-cf api api.$DOMAIN --skip-ssl-validation
-cf auth admin $CF_ADMIN_PASS
-cf target -o $ORG_NAME -s $SPACE_NAME
-
-cf create-user $USERNAME $PASSWORD
-cf set-org-role $USERNAME $ORG_NAME OrgManager
-cf set-org-role $USERNAME $ORG_NAME OrgAuditor
-cf set-org-role $USERNAME $ORG_NAME BillingManager
-cf set-space-role $USERNAME $ORG_NAME $SPACE_NAME SpaceManager
-cf set-space-role $USERNAME $ORG_NAME $SPACE_NAME SpaceDeveloper
-cf set-space-role $USERNAME $ORG_NAME $SPACE_NAME SpaceAuditor
-
-uaac target uaa.$DOMAIN --skip-ssl-validation
-ADMIN_SECRET=`cf env user-management | grep -P -o '(?<=clientSecret\"\:\s\").*(?=\")'`
+uaac target http://uaa.$DOMAIN --skip-ssl-validation
 uaac token client get admin -s $ADMIN_SECRET
-
-uaac member add cloud_controller.read $USERNAME
-uaac member add cloud_controller.write $USERNAME
-uaac member add cloud_controller.admin $USERNAME
+uaac user add $USERNAME --email $EMAIL -p $PASSWORD
 uaac member add uaa.admin $USERNAME
-uaac member add console.admin $USERNAME
-uaac member add scim.read $USERNAME
-uaac member add scim.write $USERNAME
+uaac member add tap.admin $USERNAME
