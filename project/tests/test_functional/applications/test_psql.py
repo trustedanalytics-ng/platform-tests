bf7663ecabd2b12d6714870f40d29dabc18a5208
tciunel <tomasz.ciunel@intel.com>
2016-11-07 16:05:40 +0100

    DPNG-8774 adjust sample postgress app tests to TAP NG

--- project/tests/test_functional/applications/test_psql.py
+++ project/tests/test_functional/applications/test_psql.py
@@ -16,12 +16,9 @@
 
 import pytest
 
-from modules.app_sources import AppSources
-from modules.constants import ServiceLabels, TapComponent as TAP, TapGitHub
+from modules.constants import TapComponent as TAP
 from modules.markers import priority
 from modules.service_tools.psql import PsqlTable, PsqlColumn, PsqlRow
-from modules.tap_logger import step
-from modules.tap_object_model import Application, ServiceInstance, ServiceOffering
 from modules.test_names import generate_test_object_name
 from tests.fixtures.db_input import DbInput
 
@@ -29,42 +26,11 @@ logged_components = (TAP.service_catalog,)
 pytestmark = [pytest.mark.components(TAP.service_catalog)]
 
 
-@pytest.mark.skip(reason="DPNG-8774 Adjust test_psql tests to TAP NG")
 class TestPsql(object):
     psql_app = None
     test_table_name = DbInput.test_table_name
     test_columns = DbInput.test_columns
     row_value_list = DbInput.test_rows_1
-
-    @pytest.fixture(scope="class", autouse=True)
-    def postgres_instance(self, class_context, test_org, test_space):
-        step("Create postgres service instance")
-        marketplace = ServiceOffering.get_list()
-        psql = next(service for service in marketplace if service.label == ServiceLabels.PSQL)
-        instance_name = generate_test_object_name()
-        psql_instance = ServiceInstance.api_create(
-            context=class_context,
-            org_guid=test_org.guid,
-            space_guid=test_space.guid,
-            service_label=ServiceLabels.PSQL,
-            name=instance_name,
-            service_plan_guid=psql.service_plan_guids[0]
-        )
-        return psql_instance
-
-    @classmethod
-    @pytest.fixture(scope="class", autouse=True)
-    def setup_psql_api_app(cls, test_space, login_to_cf, postgres_instance, class_context):
-        step("Get sql api app sources")
-        sql_api_sources = AppSources.get_repository(repo_name=TapGitHub.sql_api_example,
-                                                    repo_owner=TapGitHub.intel_data)
-        step("Push psql api app to cf")
-        cls.psql_app = Application.push(
-            class_context,
-            space_guid=test_space.guid, 
-            source_directory=sql_api_sources.path,
-            bound_services=(postgres_instance.name,)
-        )
 
     @pytest.fixture(scope="function", autouse=True)
     def cleanup_psql_tables(self, request):
@@ -79,26 +45,26 @@ class TestPsql(object):
         values.update({col["name"]: None for col in self.test_columns if col["name"] not in values.keys()})
         return PsqlRow(psql_app, table_name, id, values)
 
-    def _get_expected_rows(self):
+    def _get_expected_rows(self, psql_app):
         expected_rows = []
         for row_value in self.row_value_list:
-            id = PsqlRow.post(self.psql_app, self.test_table_name, row_value)
-            expected_rows.append(self._create_expected_row(self.psql_app, self.test_table_name, id, row_value))
+            id = PsqlRow.post(psql_app, self.test_table_name, row_value)
+            expected_rows.append(self._create_expected_row(psql_app, self.test_table_name, id, row_value))
         return expected_rows
 
     @priority.medium
-    def test_create_and_delete_table(self):
+    def test_create_and_delete_table(self, psql_app):
         self.__class__.test_table_name = generate_test_object_name(prefix=DbInput.test_table_name)
-        test_table = PsqlTable.post(self.psql_app, self.test_table_name, self.test_columns)
-        table_list = PsqlTable.get_list(self.psql_app)
+        test_table = PsqlTable.post(psql_app, self.test_table_name, self.test_columns)
+        table_list = PsqlTable.get_list(psql_app)
         assert test_table in table_list
         test_table.delete()
-        table_list = PsqlTable.get_list(self.psql_app)
+        table_list = PsqlTable.get_list(psql_app)
         assert test_table not in table_list
 
     @priority.medium
-    def test_get_table_columns(self):
-        test_table = PsqlTable.post(self.psql_app, self.test_table_name, self.test_columns)
+    def test_get_table_columns(self, psql_app):
+        test_table = PsqlTable.post(psql_app, self.test_table_name, self.test_columns)
         expected_columns = [PsqlColumn.from_json_definition(c) for c in self.test_columns]
         expected_columns.append(PsqlColumn("id", "INTEGER", False, None))
         columns = test_table.get_columns()
@@ -108,35 +74,35 @@ class TestPsql(object):
 
     @priority.medium
     @pytest.mark.parametrize("row_values", row_value_list)
-    def test_post_row(self, row_values):
-        PsqlTable.post(self.psql_app, self.test_table_name, self.test_columns)
-        new_row_id = PsqlRow.post(self.psql_app, self.test_table_name, row_values)
-        expected_row = self._create_expected_row(self.psql_app, self.test_table_name, new_row_id, row_values)
-        row_list = PsqlRow.get_list(self.psql_app, self.test_table_name)
+    def test_post_row(self, row_values, psql_app):
+        PsqlTable.post(psql_app, self.test_table_name, self.test_columns)
+        new_row_id = PsqlRow.post(psql_app, self.test_table_name, row_values)
+        expected_row = self._create_expected_row(psql_app, self.test_table_name, new_row_id, row_values)
+        row_list = PsqlRow.get_list(psql_app, self.test_table_name)
         assert expected_row in row_list
-        row = PsqlRow.get(self.psql_app, self.test_table_name, row_id=1)
+        row = PsqlRow.get(psql_app, self.test_table_name, row_id=1)
         assert row == expected_row
 
     @priority.low
-    def test_post_multiple_rows(self):
-        PsqlTable.post(self.psql_app, self.test_table_name, self.test_columns)
-        expected_rows = self._get_expected_rows()
-        rows = PsqlRow.get_list(self.psql_app, self.test_table_name)
+    def test_post_multiple_rows(self, psql_app):
+        PsqlTable.post(psql_app, self.test_table_name, self.test_columns)
+        expected_rows = self._get_expected_rows(psql_app)
+        rows = PsqlRow.get_list(psql_app, self.test_table_name)
         assert rows == expected_rows
 
     @priority.medium
-    def test_put_row(self):
-        PsqlTable.post(self.psql_app, self.test_table_name, self.test_columns)
-        expected_rows = self._get_expected_rows()
+    def test_put_row(self, psql_app):
+        PsqlTable.post(psql_app, self.test_table_name, self.test_columns)
+        expected_rows = self._get_expected_rows(psql_app)
         new_values = [{"column_name": "col0", "value": self.test_table_name}, {"column_name": "col2", "value": True}]
         expected_rows[1].put(new_values)
-        row = PsqlRow.get(self.psql_app, self.test_table_name, row_id=expected_rows[1].id)
+        row = PsqlRow.get(psql_app, self.test_table_name, row_id=expected_rows[1].id)
         assert expected_rows[1] == row
 
     @priority.medium
-    def test_delete_row(self):
-        PsqlTable.post(self.psql_app, self.test_table_name, self.test_columns)
-        posted_rows = self._get_expected_rows()
+    def test_delete_row(self, psql_app):
+        PsqlTable.post(psql_app, self.test_table_name, self.test_columns)
+        posted_rows = self._get_expected_rows(psql_app)
         posted_rows[1].delete()
-        rows = PsqlRow.get_list(self.psql_app, self.test_table_name)
+        rows = PsqlRow.get_list(psql_app, self.test_table_name)
         assert posted_rows[1] not in rows
