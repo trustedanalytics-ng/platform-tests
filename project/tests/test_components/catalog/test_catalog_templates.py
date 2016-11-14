8e2696304b05bbfdb04b702272965a5fc04f7165
justkantyka <justyna.kantyka@intel.com>
2016-11-14 09:16:57 +0100

    Unskip catalog tests for images and templates

--- project/tests/test_components/catalog/test_catalog_templates.py
+++ project/tests/test_components/catalog/test_catalog_templates.py
@@ -28,7 +28,6 @@ pytestmark = [pytest.mark.components(TAP.catalog)]
 
 
 @pytest.mark.usefixtures("open_tunnel")
-@pytest.mark.skip(reason="DPNG-12584 Error: Cannot parse ApiServiceInstance list: key PLAN_ID not found!")
 class TestCatalogTemplates:
 
     @priority.high
