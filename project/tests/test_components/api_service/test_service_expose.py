979328bb96c885ff1159e36da3089bee419ab0b6
Borys Stefanski <borysx.stefanski@intel.com>
2016-12-20 12:51:03 +0100

    DPNG-13688 Fix typo and document test
    * Document test_service_expose

--- project/tests/test_components/api_service/test_service_expose.py
+++ project/tests/test_components/api_service/test_service_expose.py
@@ -38,7 +38,26 @@ class TestServiceExpose:
         ServiceInstance.ensure_running(service, client=api_service_admin_client)
         return service
 
-    def test_expose_and_uxexpose_urls(self, api_service_admin_client, offering_from_python_app):
+    def test_expose_and_unexpose_urls(self, api_service_admin_client, offering_from_python_app):
+        """ Verify if is it possible to expose, unexpose service instance URLS, exposed URLS responds.
+
+            Push sample app, create new offering using image of sample app and create service instance of new offering.
+            Expose service instance URLS, then tests assert that URLS responds and present in service instance
+            credentials. After it unexpose URLS and verify if it is not in service credentials.
+
+            Input data:
+                Python application: application which is pushed on platform
+
+            Expected results:
+                It is possible to create new offering using image of pushed application. Service instance can be
+                 exposed. Exposed URLS responds and present in service instance credentials.
+
+            Steps:
+                1. Create instance of new offering
+                2. Expose URLS and check if responds
+                3. Get credentials and verify if contains exposed URLS
+                4. Unexpose URLS and verify if URLS are not in credentials
+        """
         step("Create offering from sample_python_app")
         service = offering_from_python_app
         step("Expose urls")
@@ -56,6 +75,22 @@ class TestServiceExpose:
 
     @pytest.mark.bugs("DPNG-13253 - Code 500 when exposing twice the same service")
     def test_expose_urls_twice(self, api_service_admin_client, offering_from_python_app):
+        """ Verify if try to expose exposed service results conflict.
+
+            Push sample app, create new offering using image of sample app and create service instance of new offering.
+            Expose service instance URLS and try to expose URLS once again.
+
+            Input data:
+                Offering: offering created based on python application image
+
+            Expected results:
+                It is possible to expose service URLS, second try results conflict (409 Conflict).
+
+            Steps:
+                1. Create instance of new offering
+                2. Expose URLS
+                3. Try to expose URLS once again
+        """
         step("Create offering from sample_python_app")
         service = offering_from_python_app
         step("Expose urls")
@@ -66,6 +101,23 @@ class TestServiceExpose:
                                      client=api_service_admin_client, service_id=service.id)
 
     def test_unexpose_urls_twice(self, api_service_admin_client, offering_from_python_app):
+        """ Verify if try to unexpose not exposed service results error.
+
+            Push sample app, create new offering using image of sample app and create service instance of new offering.
+            Expose service instance URLS and try to unexpose URLS once again.
+
+            Input data:
+                Offering: offering created based on python application image
+
+            Expected results:
+                It is possible to unexpose service URLS, second try results error (404 Not Found).
+
+            Steps:
+                1. Create instance of new offering
+                2. Expose URLS
+                3. Unxpose URLS
+                4. Try to unexpose URLS once again
+        """
         step("Create offering from sample_python_app")
         service = offering_from_python_app
         step("Expose urls")
@@ -79,6 +131,22 @@ class TestServiceExpose:
                                      client=api_service_admin_client, service_id=service.id, should_expose=False)
 
     def test_expose_non_existing_service(self, api_service_admin_client, offering_from_python_app):
+        """ Verify if try to expose non existing service instance results error
+
+            Push sample app, create new offering using image of sample app and create service instance of new offering.
+            Try to expose non existing service instance.
+
+            Input data:
+                Offering: offering created based on python application image
+                Not existing instance id: created id to expose try
+
+            Expected results:
+                Try to expose non existing service instance results error (404 Not Found)
+
+            Steps:
+                1. Create instance of new offering
+                2. Try to expose non existing service instance
+            """
         step("Create offering from sample_python_app")
         offering_from_python_app
         step("Create invalid service id")
