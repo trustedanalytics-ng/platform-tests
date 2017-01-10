07e838d9d8a006ad41f34c4ba500ed3314387663
Tomasz Groch <tomaszx.groch@intel.com>
2017-01-10 16:45:41 +0100

    DPNG-14865 [api-tests] Adjust response msg in test_cannot_scale_application_with_negative_instances_number

--- project/modules/constants/http_status.py
+++ project/modules/constants/http_status.py
@@ -246,7 +246,7 @@ class ApiServiceHttpStatus(HttpStatus):
     MSG_FIELD_INCORRECT_VALUE = "Field: {} has incorrect value"
     MSG_FIELD_ZERO_VALUE = "{}: zero value"
     MSG_PLAN_CANNOT_BE_FOUND = "plan with id: {} cannot be found"
-    MSG_MINIMUM_ALLOWED_REPLICA = "Minimum allowed replication is 0"
+    MSG_MINIMUM_ALLOWED_REPLICA = "Replicas: less than min"
 
 
 class ContainerBrokerHttpStatus(HttpStatus):
