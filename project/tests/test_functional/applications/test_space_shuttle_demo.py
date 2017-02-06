d7e5e7339e8152d6edbdcd0b648f8dbc7fc664a6
Jacob27 <jacek.skowron@intel.com>
2017-02-06 14:50:00 +0100

    DPNG-13514 space shuttle anomaly detection

--- project/tests/test_functional/applications/test_space_shuttle_demo.py
+++ project/tests/test_functional/applications/test_space_shuttle_demo.py
@@ -20,7 +20,9 @@ from modules.constants import ServiceLabels, TapEntityState, TapComponent as TAP
 from modules.markers import incremental, priority
 from modules.tap_logger import step
 from modules.tap_object_model import Application, ServiceInstance
-from retry import retry
+from modules.http_client.http_client_factory import HttpClientFactory
+from modules.http_client.configuration_provider.application import ApplicationConfigurationProvider
+from modules.http_client import HttpMethod
 
 logged_components = (TAP.space_shuttle_demo,)
 pytestmark = [pytest.mark.components(TAP.space_shuttle_demo)]
@@ -32,6 +34,7 @@ class TestSpaceShuttleDemo:
     SPACE_SHUTTLE_DEMO_APP_NAME = "space-shuttle-demo"
     SPACE_SHUTTLE_DEMO_CLIENT_NAME = "space-shuttle-demo-client"
     SPACE_SHUTTLE_GATEWAY = "space-shuttle-gateway"
+    SPACE_SHUTTLE_SCORING_ENGINE = "space-shuttle-scoring-engine"
     EXPECTED_BINDINGS = [ServiceLabels.GATEWAY, ServiceLabels.INFLUX_DB_088, ServiceLabels.SCORING_ENGINE]
 
     @pytest.fixture(scope="class")
@@ -127,40 +130,38 @@ class TestSpaceShuttleDemo:
         assert space_shuttle_client.state == TapEntityState.RUNNING
 
     @priority.low
+    @pytest.mark.bugs("DPNG-15302 500 Error while proxying request to service api, path /rest/services/")
     def test_5_check_if_client_sending_data_to_gateway(self, space_shuttle_client):
         application_log = space_shuttle_client.get_logs()
         assert self.check_that_logs_contain_client_data(application_log)
 
     @priority.low
+    @pytest.mark.bugs("DPNG-15302 500 Error while proxying request to service api, path /rest/services/")
     def test_6_check_if_gateway_receiving_data_from_client(self, space_shuttle_gateway):
         service_log = space_shuttle_gateway.get_logs()
         assert self.check_that_logs_contain_client_data(service_log)
 
-    @pytest.mark.skip(reason="DPNG-9278 [space shuttle demo] Enable usage os Scoring Engine")
-    def test_2_check_app_model_dataset_title_and_uri(self, core_org):
+    def test_7_check_model_presence_in_scoring_engine(self):
         """
         <b>Description:</b>
-        Checks if space-shuttle-demo application has correct dataset title and uri.
+        Checks if Scoring Engine can return information if provided data is anomaly basing on model.
 
         <b>Input data:</b>
-        1. Organization
+        1. 9 digits representing lack of anomaly.
 
         <b>Expected results:</b>
-        The application has correct a dataset and a title.
+        The application can return information if provided data is anomaly basing on model.
 
         <b>Steps:</b>
-        1. Verify the title.
-        2. Verify the uri.
+        1. Check if model is present in Scoring Engine.
         """
-        step('Check model title is as expected')
-        dataset_list = DataSet.api_get_list(org_guid_list=[core_org.guid])
-        model = next((d for d in dataset_list if d.title == self.MODEL_TITLE), None)
-        assert model is not None, 'Model dataset is not present'
-        step('Check model uri is as expected')
-        assert self.MODEL_SOURCE_FILE_NAME == model.source_uri, 'Model source file name is not as expected'
+        step('Check if model is present in Scoring Engine')
+        scoring_engine = ServiceInstance.get_by_name(self.SPACE_SHUTTLE_SCORING_ENGINE)
+        client = HttpClientFactory.get(ApplicationConfigurationProvider.get(url=scoring_engine.url))
+        response = client.request(method=HttpMethod.GET, params="data=1,1,1,1,1,1,1,1,1", path="v1/score")
+        assert response == 1.0, "Scoring Engine model has changed"
 
-    @pytest.mark.skip(reason="DPNG-9278 [space shuttle demo] Enable usage os Scoring Engine")
-    def test_3_space_shuttle_anomaly_detection(self, space_shuttle_demo_app, space_shuttle_client_app):
+    def test_8_space_shuttle_anomaly_detection(self, space_shuttle_application):
         """
         <b>Description:</b>
         Checks if space-shuttle-demo application anomalies are detected correctly.
@@ -176,38 +177,8 @@ class TestSpaceShuttleDemo:
         1. Post samples.
         2. Verify the anomalies.
         """
-        step('Check that anomalies are detected correctly')
-        now = time.time()
-        an_hour_ago = now - 3600
-        interval = '60m'
-        step("Check how long the client works")
-        current_utc_date_and_time = datetime.utcnow()
-        space_shuttle_client_package_updated_at_utc = datetime.strptime(space_shuttle_client_app.api_get_summary()
-                                                                        ["package_updated_at"], '%Y-%m-%dT%H:%M:%SZ')
-        space_shuttle_client_up_time = current_utc_date_and_time - space_shuttle_client_package_updated_at_utc
-        space_shuttle_client_up_time = space_shuttle_client_up_time.seconds
-        '''
-        Test may fail if application was started less than hour before.
-        Time in Application.api_get_summary["package_updated_at"] is UTC
-        '''
-        interval_start = str(int(an_hour_ago * 1000))
-        step("Get samples data")
-        samples_data = space_shuttle_demo.post_samples(space_shuttle_demo_app, interval, interval_start)
-        step("If up time is less than 1h, check that any data are returned")
-        if space_shuttle_client_up_time < 3600:
-            assert len(samples_data) > 0
-        step("If up time is more than 1h, check if anomalies are present for 90% of time")
-        if space_shuttle_client_up_time > 3600:
-            percent_correct = 90
-        else:
-            percent_correct = 70
-        '''
-        There are 10 classes of anomalies. The correct one is class=5. Test checks the quality of algorithm
-        by comparing the most populated anomaly class (probably 5) against the sum of all anomalies detected.
-        percent_correct value (70) is chosen to pass most of the tests (it is not critical test)
-        '''
-        quantity_of_class_anomalies = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        for _, anomaly in samples_data.items():
-            anomaly_class = int(anomaly[1])
-            quantity_of_class_anomalies[anomaly_class] += 1
-        assert max(quantity_of_class_anomalies) * 100 / sum(quantity_of_class_anomalies) > percent_correct
+        step('Check if anomalies are detected')
+        client = HttpClientFactory.get(ApplicationConfigurationProvider.get(url=space_shuttle_application.urls[0]))
+        sample_data = client.request(method=HttpMethod.GET, path="rest/space-shuttle/chart", params={"groupBy": "1m",
+                                                                                                     "since": "1h"})
+        assert len(sample_data) > 0
